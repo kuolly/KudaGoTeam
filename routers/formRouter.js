305dd91319e7614a1119e03be7da5f363b223225
Danya <danya.culakov@yandex.ru>
2021-05-14 15:41:33 +0300

    rel 9

--- routers/formRouter.js
+++ routers/formRouter.js
@@ -17,10 +17,27 @@ router.get("/events", async (req, res) => {
 router.post("/events", async (req, res) => {
   req.body.date = new Date();
   req.body.tag = req.body.tag.split(",");
-  await eventModel.create(req.body);
-  await tagModel.create(req.body);
+  await Event.create(req.body);
+  await Tag.create(req.body);
   res.redirect("/form/events"); 
 });
+
+// router.post("/form", async (req, res) => {
+//   const { fruit, vegetables, makaroni } = req.body;
+//   console.log(req.body);
+//   if (fruit && vegetables && makaroni) {
+//     const currentForm = await formModel.create({ fruit, vegetables, makaroni });
+//     console.log(currentForm);
+//     if (currentForm) {
+//       return res.status(200).redirect("/");
+//     }
+//     return res.status(418).redirect("/user/form");
+//   }
+//   return res.status(418).redirect("/user/form");
+// });
+
+
+
 
 
 module.exports = router;
